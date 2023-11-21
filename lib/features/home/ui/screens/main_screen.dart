531a70d57bebbea31ab0fd5799b3d1245d880013
DESKTOP-7C253GF\Eng MoHego <moashrafasd12@gmail.com>
2023-11-21 23:12:32 +0200

    add bottomNavigationBar and navigate between screens

--- lib/features/home/ui/screens/main_screen.dart
+++ lib/features/home/ui/screens/main_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:laza/core/theming/color/my_color.dart';
+import 'package:laza/features/home/ui/screens/card_screen.dart';
+import 'package:laza/features/home/ui/screens/home_screen.dart';
+import 'package:laza/features/home/ui/screens/order_screen.dart';
+import 'package:laza/features/home/ui/screens/wishlist_screen.dart';
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -9,16 +13,29 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+  int currentTap = 0;
+  final List<Widget> screens = [
+    const HomeScreen(),
+    const WishListScreen(),
+    const OrderScreen(),
+    const CardScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: screens[currentTap],
       bottomNavigationBar: Container(
         color: Colors.white,
         child:  Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 8),
           child: GNav(
+            selectedIndex: currentTap,
             onTabChange: (index){
-              print(index);
+              setState(() {
+                currentTap = index;
+              });
             },
             gap: 8,
             tabBackgroundColor: MyColor.mainColor,
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:laza/features/home/ui/screens/home_screen.dart';
 import 'package:laza/features/home/ui/screens/main_screen.dart';
 
 void main() {
